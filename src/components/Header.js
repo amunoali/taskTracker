204939f21d40735f2b815c7e215b17750dbb49c2
amunoali <amunoali@gmail.com>
2022-08-19 11:58:53 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+      <h2>{title}</h2>
+      <Button color={showAdd ? 'red' : 'green'} text= {showAdd ? 'Close' : 'Add'} onClick=  {onAdd}  />
+      
+    </header>
+  )
+}
+Header.defaultProps = {
+    title: 'Task Tracker', 
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header 
